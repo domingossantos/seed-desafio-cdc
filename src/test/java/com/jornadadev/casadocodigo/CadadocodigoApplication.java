b81467f304ed9e1b1faa00819781f522d5ee2670
Domingos Santos <domsantos@gmail.com>
2021-03-24 22:29:46 -0300

    Cadastro de Autor

---
+++ src/test/java/com/jornadadev/casadocodigo/CadadocodigoApplication.java
@@ -0,0 +1,12 @@
+package com.jornadadev.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadadocodigoApplication.class, args);
+	}
+}
